c7ae08c1390a4bf3989075d37d692de0036319ca
Dani <danisweetwood@gmail.com>
2023-11-28 23:46:37 -0600

    Add files via upload

--- logo.js
+++ logo.js
@@ -28,13 +28,19 @@ inquirer
   ])
   .then((data) => {
     const {text, textColor, shape, shapeColor} = data;
+    if (shape == 'circle'){
+      var shapeCode = `circle cx='120' cy='100' r='80'`;
+    } else if (shape == 'square'){
+      var shapeCode = `rect x='70' y='230' width='160' height='160'`;
+    } else var shapeCode = `polygon points='0,200 150,0 300,200'`; 
+
     fs.writeFile('logo.svg', `
     <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-    <${shape} cx="150" cy="100" r="80" fill="${shapeColor}" />
+    <${shapeCode} fill="${shapeColor}" />
   
     <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
   
     </svg>`
-    , (err) => (err) ? console.error(err) : console.log('Writing README!'))
+    , (err) => (err) ? console.error(err) : console.log('Creating your new logo!'))
     });
